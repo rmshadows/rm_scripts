d292c9b506804251890c2be91132129f6a9dfb38
ryan-cahs-workgroup-pc <justaaaa@163.com>
2022-03-11 05:32:01 +0800

    update

--- Debian_GNOME_Init/Debian_Bullseye_GNOME_Setup.sh
+++ Debian_GNOME_Init/Debian_Bullseye_GNOME_Setup.sh
@@ -2,7 +2,7 @@
 # https://github.com/rmshadows/rm_scripts
 
 :<<!说明
-Version：0.0.6
+Version：0.0.7
 预设参数（在这里修改预设参数, 谢谢）
 注意：如果没有注释，默认0 为否 1 为是。
 if [ "$" -eq 1 ];then
@@ -1123,11 +1123,11 @@ RIME_DICT_HEADER="# luna_pinyin.custom.yaml
 #
 #
 # 附朙月拼音系列方案與其對應的 id 一覽表：
-# 輸入方案	id
-# 朙月拼音	luna_pinyin
-# 朙月拼音·简化字	luna_pinyin_simp
-# 朙月拼音·臺灣正體	luna_pinyin_tw
-# 朙月拼音·語句流	luna_pinyin_fluency
+# 輸入方案      id
+# 朙月拼音      luna_pinyin
+# 朙月拼音·简化字      luna_pinyin_simp
+# 朙月拼音·臺灣正體      luna_pinyin_tw
+# 朙月拼音·語句流      luna_pinyin_fluency
 #
 
 patch:
@@ -1152,8 +1152,8 @@ import_tables:
 ...
 
 # 在下面添加用户词库
-举个例子	jugelizi	1
-测试词组	cscz
+举个例子      jugelizi      1
+测试词组      cscz
 
 "
 
@@ -1174,6 +1174,133 @@ _________  .___ ____   ____.___ _________  _________  _________  _________  ____
 ==========================================
 
 \e[0m"
+
+# 检测到苹果系统，退出
+if [ -e /usr/bin/uname ]; then
+    if [ "`/usr/bin/uname -s`" = "Darwin" ]; then
+        prompt -e '*** Detected MacOS / Darwin, quit!'
+        prompt -e '*** Linux only!'
+        exit 0
+    fi
+fi
+
+# 检测Linux发行版
+prompt -w '*** Detecting Linux Distribution....'
+if [ -f /etc/debian_version ]; then
+    dvers=`cat /etc/debian_version | cut -d '.' -f 1 | cut -d '/' -f 1`
+    if [ -f /etc/lsb-release -a -n "`cat /etc/lsb-release 2>/dev/null | grep -F -i LinuxMint`" ]; then
+        # Linux Mint -> Ubuntu 'xenial'
+        prompt -w '*** Found Linux Mint      Ubuntu xenial'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ -f /etc/lsb-release -a -n "`cat /etc/lsb-release 2>/dev/null | grep -F trusty`" ]; then
+        # Ubuntu 'trusty'
+        prompt -w '*** Found Ubuntu      trusty'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ -f /etc/lsb-release -a -n "`cat /etc/lsb-release 2>/dev/null | grep -F wily`" ]; then
+        # Ubuntu 'wily'
+        prompt -w '*** Found Ubuntu      wily'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ -f /etc/lsb-release -a -n "`cat /etc/lsb-release 2>/dev/null | grep -F xenial`" ]; then
+        # Ubuntu 'xenial'
+        prompt -w '*** Found Ubuntu      xenial'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ -f /etc/lsb-release -a -n "`cat /etc/lsb-release 2>/dev/null | grep -F zesty`" ]; then
+        # Ubuntu 'zesty'
+        prompt -w '*** Found Ubuntu      zesty'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ -f /etc/lsb-release -a -n "`cat /etc/lsb-release 2>/dev/null | grep -F precise`" ]; then
+        # Ubuntu 'precise'
+        prompt -w '*** Found Ubuntu      recise'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ -f /etc/lsb-release -a -n "`cat /etc/lsb-release 2>/dev/null | grep -F artful`" ]; then
+        # Ubuntu 'artful'
+        prompt -w '*** Found Ubuntu      artful'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ -f /etc/lsb-release -a -n "`cat /etc/lsb-release 2>/dev/null | grep -F bionic`" ]; then
+        # Ubuntu 'bionic'
+        prompt -w '*** Found Ubuntu      bionic'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ -f /etc/lsb-release -a -n "`cat /etc/lsb-release 2>/dev/null | grep -F yakkety`" ]; then
+        # Ubuntu 'yakkety'
+        prompt -w '*** Found Ubuntu      yakkety'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ -f /etc/lsb-release -a -n "`cat /etc/lsb-release 2>/dev/null | grep -F disco`" ]; then
+        # Ubuntu 'disco'
+        prompt -w '*** Found Ubuntu      disco'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ -f /etc/lsb-release -a -n "`cat /etc/lsb-release 2>/dev/null | grep -F focal`" ]; then
+        # Ubuntu 'focal' -> Ubuntu 'bionic' (for now)
+        prompt -w '*** Found Ubuntu      focal->bionic'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ -f /etc/lsb-release -a -n "`cat /etc/lsb-release 2>/dev/null | grep -F hirsute`" ]; then
+        # Ubuntu 'hirsute' -> Ubuntu 'bionic' (for now)
+        prompt -w '*** Found Ubuntu      hirsute->bionic'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ -f /etc/lsb-release -a -n "`cat /etc/lsb-release 2>/dev/null | grep -F impish`" ]; then
+        # Ubuntu 'impish' -> Ubuntu 'bionic' (for now)
+        prompt -w '*** Found Ubuntu      impish->bionic'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ "$dvers" = "6" -o "$dvers" = "squeeze" ]; then
+        # Debian 'squeeze'
+        prompt -w '*** Found Debian      squeeze'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ "$dvers" = "7" -o "$dvers" = "wheezy" ]; then
+        # Debian 'wheezy'
+        prompt -w '*** Found Debian      wheezy'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ "$dvers" = "8" -o "$dvers" = "jessie" ]; then
+        # Debian 'jessie'
+        prompt -w '*** Found Debian      jessie'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ "$dvers" = "9" -o "$dvers" = "stretch" ]; then
+        # Debian 'stretch'
+        prompt -w '*** Found Debian      '
+    elif [ "$dvers" = "10" -o "$dvers" = "buster" -o "$dvers" = "parrot" ]; then
+        # Debian 'buster'
+        prompt -w '*** Found Debian      buster'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    elif [ "$dvers" = "11" -o "$dvers" = "bullseye" ]; then
+        # Debian 'bullseye'
+        prompt -s '*** Found Debian      bullseye'
+    elif [ "$dvers" = "testing" -o "$dvers" = "sid" -o "$dvers" = "bookworm" ]; then
+        # Debian 'testing', 'sid', and 'bookworm' -> Debian 'bookworm'
+        prompt -s '*** Found Debian      testing'
+        prompt -w "*** WARN: Debian testing 或 sid，请谨慎使用！"
+    else
+        # Use Debian "buster" for unrecognized Debians
+        prompt -w '*** Found Debian or Debian derivative      unrecognized Debians'
+        prompt -w "*** WARN: 非本脚本指定的发行版，谨慎使用！ Not for this distribution, use with caution !"
+    fi
+elif [ -f /etc/SuSE-release -o -f /etc/suse-release -o -f /etc/SUSE-brand -o -f /etc/SuSE-brand -o -f /etc/suse-brand ]; then
+    prompt -e '*** Found SuSE, required zypper YUM repo...'
+    prompt -e '*** Debian only !'
+    exit 1
+elif [ -d /etc/yum.repos.d ]; then
+    if [ -n "`cat /etc/redhat-release 2>/dev/null | grep -i fedora`" ]; then
+        prompt -e "*** Found Fedora, required /etc/yum.repos.d/xxx.repo"
+        prompt -e '*** Debian only !'
+        exit 1
+    elif [ -n "`cat /etc/redhat-release 2>/dev/null | grep -i centos`" -o -n "`cat /etc/redhat-release 2>/dev/null | grep -i enterprise`" ]; then
+        prompt -e "*** Found RHEL/CentOS, required /etc/yum.repos.d/xxx.repo"
+        prompt -e '*** Debian only !'
+        exit 1
+    elif [ -n "`cat /etc/system-release 2>/dev/null | grep -i amazon`" ]; then
+        prompt -e "*** Found Amazon (CentOS/RHEL based), required /etc/yum.repos.d/xxx.repo"
+        if [ -n "`cat /etc/system-release 2>/dev/null | grep -F 'Amazon Linux 2'`" ]; then
+            prompt -e '*** Debian only, NOT redhat/el/7 !'
+            exit 1
+        else
+            prompt -e '*** Debian only, NOT redhat/amzn1/2016.03 !'
+            exit 1
+        fi
+    else
+        prompt -e "*** Found unknown yum-based repo, using el/7"
+        prompt -e '*** Debian only !'
+        exit 1
+    fi
+fi
+
+
 # R
 echo -e "\e[1;31m接下来请根据提示进行操作，正在准备(1s)...\n\e[0m"
 sleep 1
