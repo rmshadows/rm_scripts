57656c9d02bdadfcca70d6dd1e4b28f67344a32c
jessie <@blanc-AL003>
2022-04-06 12:53:53 +0800

    add and update

---
+++ Windows/OpenApplicationDownloadPages.bat
@@ -0,0 +1,59 @@
+REM QQ
+start https://im.qq.com/pcqq
+
+REM Wechat https://weixin.qq.com/
+start https://pc.weixin.qq.com/
+
+REM VS Code
+start https://code.visualstudio.com/#alt-downloads
+
+REM Java 11
+start https://www.oracle.com/java/technologies/downloads/#java11-windows
+
+REM Python
+REM  python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
+REM  pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+start https://www.python.org/downloads/
+
+REM PyCharm
+start https://www.jetbrains.com/pycharm/download/#section=windows
+
+REM Virtualbox
+REM start https://www.virtualbox.org/
+
+REM Chrome
+start https://www.google.cn/intl/zh-CN/chrome/
+
+REM Firefox
+REM start https://www.mozilla.org/zh-CN/firefox/all/#product-desktop-esr
+
+REM Eclipse
+start https://www.eclipse.org/downloads/packages/
+
+REM Visual Studio
+start https://visualstudio.microsoft.com/zh-hans/
+
+REM 迅雷
+start https://www.xunlei.com/
+
+REM 360
+start https://weishi.360.cn/?source=homepage/
+start https://yasuo.360.cn/
+
+REM  zerotier
+https://www.zerotier.com/
+
+REM WPS
+start https://platform.wps.cn/
+
+
+
+
+
+
+
+
+
+
+
+
