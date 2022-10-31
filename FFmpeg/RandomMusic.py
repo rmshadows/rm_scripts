f7a846b49b7bd4eecbdf1f4e25fcec24ccc1a1bb
jessie <@blanc-AL003>
2022-10-31 18:45:38 +0800

    add and update ———— 2022年 10月 31日 星期一 18:45:38 CST

--- FFmpeg/RandomMusic.py
+++ FFmpeg/RandomMusic.py
@@ -8,28 +8,37 @@ import os
 import random
 
 # 音乐文件夹名称
-MUSIC_DIR = "Music"
+MD = ["", ""]
+# 重复
+MNUM = 2
 
-file_lst = os.listdir(MUSIC_DIR)
-# 去掉py txt 隐藏文件
-for i in file_lst:
-    if i.endswith("py") or i.endswith("txt"):
-        file_lst.remove(i)
-    if i[0:1] == ".":
-        file_lst.remove(i)
-# 扩展名
-extn = os.path.splitext(file_lst[4])[-1]
-print(extn)
-random.shuffle(file_lst)
-# print(file_lst)
-cmd = ""
-for i in file_lst:
-    cmd += "file '{}'\n".format(os.path.join(MUSIC_DIR, i))
-print(cmd)
-txt = str(datetime.now())[-6:-1] + ".txt"
-with open(txt, "w", encoding="utf8") as f:
-    f.write(cmd)
-cmd = "ffmpeg -f concat -i {} -c copy {}{}".format(txt, txt.replace(".txt", ""), extn)
-print(cmd)
-os.popen(cmd)
+def RandomMusic(MUSIC_DIR):
+    file_lst = os.listdir(MUSIC_DIR)
+    # 去掉py txt 隐藏文件
+    for i in file_lst:
+        if i.endswith("py") or i.endswith("txt"):
+            file_lst.remove(i)
+        if i[0:1] == ".":
+            file_lst.remove(i)
+    # 扩展名
+    extn = os.path.splitext(file_lst[4])[-1]
+    print(extn)
+    random.shuffle(file_lst)
+    # print(file_lst)
+    cmd = ""
+    for i in file_lst:
+        cmd += "file '{}'\n".format(os.path.join(MUSIC_DIR, i))
+    print(cmd)
+    txt = "{}-{}.txt".format(MUSIC_DIR, str(datetime.now())[-6:-1])
+    with open(txt, "w", encoding="utf8") as f:
+        f.write(cmd)
+    cmd = "ffmpeg -f concat -i {} -c copy {}{}".format(txt, txt.replace(".txt", ""), extn)
+    print(cmd)
+    os.popen(cmd)
 
+
+if __name__ == '__main__':
+    for d in MD:
+        for i in range(0, MNUM):
+            RandomMusic(d)
+    
