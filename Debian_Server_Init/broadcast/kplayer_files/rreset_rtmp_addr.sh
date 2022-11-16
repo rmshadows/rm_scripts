fa1f6e510c1ad26d689d32372185219dcc21f092
jessie <@blanc-AL003>
2022-11-16 13:02:46 +0800

    add and update ———— 2022年 11月 16日 星期三 13:02:46 CST

--- Debian_Server_Init/broadcast/kplayer_files/rreset_rtmp_addr.sh
+++ Debian_Server_Init/broadcast/kplayer_files/rreset_rtmp_addr.sh
@@ -3,3 +3,5 @@
 sed -i '1d' RTMP.txt
 sed -i '1i RTMP_ADDR=""' RTMP.txt
 nano RTMP.txt
+# 直接重新设置RTMP地址
+set_rtmp_addr.sh
--- Debian_Server_Init/broadcast/kplayer_files/set_rtmp_addr.sh
+++ Debian_Server_Init/broadcast/kplayer_files/set_rtmp_addr.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # 获取空格
 source "RTMP.txt"
+source "kconfig.sh"
 echo "$RTMP_ADDR"
+echo "$KCONF"
 check_var="\"path\": \""
-space_prefix=`cat config.json | grep "$check_var"`
+space_prefix=`cat "$KCONF" | grep "$check_var"`
 space_prefix=`echo "$space_prefix" | sed s/\ /！/g`
 space_prefix=($(echo "$space_prefix" | tr "\"" "\n"))
 space_prefix=${space_prefix[0]}
-idx=`cat config.json | grep -n "$check_var" | gawk '{print $1}' FS=":"`
-sed -i "$idx d" config.json
+idx=`cat "$KCONF" | grep -n "$check_var" | gawk '{print $1}' FS=":"`
+sed -i "$idx d" "$KCONF"
 RTMP_ADDR="$space_prefix\"path\": \"$RTMP_ADDR\""
-sed -i "$idx i$RTMP_ADDR" config.json
-sed -i s/！/\ /g config.json
+sed -i "$idx i$RTMP_ADDR" "$KCONF"
+sed -i s/！/\ /g "$KCONF"
 
 
 
