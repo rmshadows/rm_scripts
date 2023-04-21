272043d46206a8f800b6dc59685fb0ef074c2599
jessie <justaaaa@163.com>
2023-04-21 22:34:02 +0800

    add and update ———— 2023年 04月 21日 星期五 22:34:00 CST

--- FFmpeg/FfmpegCut.sh
+++ FFmpeg/FfmpegCut.sh
@@ -7,9 +7,7 @@ V_FILE="1.MP4"
 # 输出文件名
 OUTPUT_NAME="OUT1.MP4"
 
-ffmpeg -ss "$FROM" -i "$V_FILE" -to "$TO" -vcodec copy -acodec copy "$OUTPUT_NAME"
-# 下面这句效果一样，只是视频搜寻速度不同
-# ffmpeg -i "$V_FILE" -ss "$FROM" -to "$TO" -vcodec copy -acodec copy "$OUTPUT_NAME"
+ffmpeg -i "$V_FILE" -ss "$FROM" -to "$TO" -vcodec copy -acodec copy "$OUTPUT_NAME"
 # 如果上面那一句出错的话
 # ffmpeg -i "$V_FILE" -ss "$FROM" -t "$TO" -c copy "$OUTPUT_NAME"
 
