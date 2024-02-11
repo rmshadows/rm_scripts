025ab07f5e357514f79a88c19652cf6cde61c8ca
jessie <justaaaa@163.com>
2024-02-11 23:02:14 +0800

    add and update ———— 2024年 02月 11日 星期日 23:02:14 CST

--- JavaReleaseJpackage/Build.bat
+++ JavaReleaseJpackage/Build.bat
@@ -1,13 +1,12 @@
 @echo off
 REM 输出文件夹名称 (Textsend-4.0.5)
-SET OUTPUT_NAME=""
+SET OUTPUT_NAME="Textsend-4.0.5"
 REM 可执行的Jar文件名称(如：Textsend-4.0.5.jar)
-SET JAR_FILENAME=".jar"
+SET JAR_FILENAME="Textsend-4.0.5.jar"
 REM 主类（如：application.TextSendMain）
-SET MAIN_CLASS=""
+SET MAIN_CLASS="application.TextSendMain"
 REM 图标
-SET ICON=""
+SET ICON="icon.ico"
 
 REM RunnableJarFile文件夹中要放入所有要打包的执行相关资源
 jpackage --input .\RunnableJarFile --name "%OUTPUT_NAME%" --main-jar "%JAR_FILENAME%" --main-class "%MAIN_CLASS%" --type app-image --icon "%ICON%"
-
